578ab32a7140515e4274e00d5ff6063b3a28321f
Lane1655 <lane.fiedler@outlook.com>
2024-04-19 16:44:07 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import time
+
+H = "hello"
+Hello = "H"
+
+# Says Hello
+
+print(H)
+
+print("""What
+      is 
+      your
+      name
+      ?""")
+
+name = input("Enter your name: ")
+
+time.sleep(4)
+
+print(H + " " + name + " Nice to have you here today :)")
+
+time.sleep(2)
+
+exit()
